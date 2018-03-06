70f78a07519ffb4de6da3d9416c5a75cdbfc5256
ciel <user@192.168.219.102>
2018-03-06 18:57:31 +0900

    *[문자열사용하기] 상수
    -세 자리의 숫자를 두 개 받아 첫 번째 자리의 숫자와 3번 째 자리의 숫자를 바꾸고 두 개의 숫자를 대소비교하여 큰 수 출력

---
+++ Algorithm/SSnumm.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class SSnumm {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String num1 = scan.next();
+		String num2 = scan.next();
+		
+		StringBuilder sb = new StringBuilder(num1);
+		StringBuilder sb2 = new StringBuilder(num2);
+		
+		sb.setCharAt(0, num1.charAt(2));
+		sb.setCharAt(2, num1.charAt(0));
+		sb2.setCharAt(0, num2.charAt(2));
+		sb2.setCharAt(2, num2.charAt(0));
+		
+		String numnum = sb.toString();
+		String numnum2 = sb2.toString();
+		
+		int numnumnum = Integer.parseInt(numnum);
+		int numnumnum2 = Integer.parseInt(numnum2);
+		
+		if(numnumnum>numnumnum2) {
+			System.out.println(numnumnum);
+		} else {
+			System.out.println(numnumnum2);
+		}
+		
+		scan.close();
+	}
+}
