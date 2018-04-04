5ea47b69a099dc82390d9c6514cfeeebe1a0f923
ciel <user@192.168.219.102>
2018-04-04 18:21:26 +0900

    * [문자열사용하기] 가장 많이 사용된 알파벳 찾기

---
+++ Algorithm/VocaStudy.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class VocaStudy {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String voca = scan.next();
+		String VOCA = voca.toLowerCase();
+		int num [] = new int [26];
+		char vocas [] = new char [voca.length()];
+		char alpa [] = {'A', 'B', 'C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+		int result = 0;
+		int max = num[0];
+		int maxx = 0; 
+		int b = 0;
+		boolean a = false;
+		for(int i=0 ; i<voca.length() ; i++) {
+			vocas[i] = VOCA.charAt(i);
+			result = vocas[i] - 'a';
+			num[result]++;
+			if(max < num[result]) {
+				max = num[result];
+				maxx = result;
+				
+			}
+		}
+		
+		for(int i=0 ; i<26 ; i++) {
+			if(max == num[i]) {
+				b++;
+				if( b>1) {
+					a=true;
+				}
+			}
+		}
+		if(a == true) {
+			System.out.println("?");
+		} else {
+			System.out.println(alpa[maxx]);
+		}
+		
+
+		
+		
+		scan.close();
+	}
+}
